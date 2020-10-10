8b7deb1944dbe636543cb61db9fca7c8e03a25da
Bryan Flynt <bryan.flynt@gmail.com>
2020-10-09 20:20:21 -0600

    Added tuple header

---
+++ include/xstd/tuple.hpp
@@ -0,0 +1,15 @@
+/*
+ * tuple.hpp
+ *
+ *  Created on: Dec 9, 2019
+ *      Author: bflynt
+ */
+
+#ifndef INCLUDE_XSTD_TUPLE_HPP_
+#define INCLUDE_XSTD_TUPLE_HPP_
+
+#include "xstd/detail/tuple/algorithm.hpp"
+#include "xstd/detail/tuple/functional.hpp"
+
+
+#endif /* INCLUDE_XSTD_TUPLE_HPP_ */
